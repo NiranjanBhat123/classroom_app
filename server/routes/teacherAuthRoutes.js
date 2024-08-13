9ef93e24c9404dd97bbfeeabec5cbf59761f0e81
Niranjan bhat <111105882+NiranjanBhat123@users.noreply.github.com>
2024-08-13 15:37:31 +0530

    bug fixes

--- server/routes/teacherAuthRoutes.js
+++ server/routes/teacherAuthRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/TeacherauthController');
+const { login } = require('../controllers/TeacherAuthController');
 
 router.post('/login', login);
 
